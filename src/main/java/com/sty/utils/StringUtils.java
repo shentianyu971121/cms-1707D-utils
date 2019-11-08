0b7631daf5128c4355b12b6f1f03f7ad6ede3acf
shentianyu971121 <948653027@qq.com>
2019-11-08 11:28:18 +0800

    二次添加

--- src/main/java/com/sty/utils/StringUtils.java
+++ src/main/java/com/sty/utils/StringUtils.java
@@ -1,5 +1,13 @@
 package com.sty.utils;
 
 public class StringUtils {
-
+	
+	public static boolean haveValue(String str) {
+		return !(str == null || str.trim().equals(""));
+	}
+	
+	
+	public static void isMobile(String str) {
+		
+	}
 }
---
+++ src/test/java/com/sty/test/TestStringUtils.java
@@ -0,0 +1,13 @@
+package com.sty.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TestStringUtils {
+	
+	@Test
+	public void testIsBlank() {
+		Assert.assertTrue(1==2);
+	}
+}
